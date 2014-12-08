52fa7ee4d4bdd9c13d313d57a384978b67c902e5
Simon Yousoufov <simon.yous@gmail.com>
2014-12-07 20:27:50 -0500

    __init__.py files ran away

--- CroppingService/wsgi.py
+++ CroppingService/wsgi.py
@@ -8,8 +8,8 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os, sys
-# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "CroppingService.settings")
-sys.path.append(os.path.join(os.path.dirname(__file__)))
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "CroppingService.settings")
+# sys.path.append(os.path.join(os.path.dirname(__file__)))
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
